39e7ab089adb5c5f45aa1ba59ee1cd3a5dc25aa5
Alex Niznik <aniznik@cs.utexas.edu>
2017-03-22 14:54:31 -0500

    Populated db

--- app/main.py
+++ app/main.py
@@ -65,15 +65,15 @@ def about():
 
 @app.route('/games')
 def games():
-	return render_template("games.html",  games=Game.query.all(), publishers=Publisher.query.all())
+	return render_template("games.html", games = Game.query.all(), )
 
 @app.route('/genre')
 def genre():
-	return render_template("genre.html")
+	return render_template("genre.html", genres = Genre.query.all())
 
 @app.route('/publisher')
 def publisher():
-	return render_template("publisher.html")
+	return render_template("publisher.html", publishers = Publisher.query.all())
 
 if __name__ == "__main__":
 
@@ -84,15 +84,64 @@ if __name__ == "__main__":
 	game.year = 2015
 	game.theme = "Action RPG"
 	game.publisher = "CD Projekt RED"
+	db.session.add(game)
+
+	game = Game()
+	game.name = "Dark Souls III"
+	game.year = 2016
+	game.theme = "Action RPG"
+	game.publisher = "Bandai Namco"
+	db.session.add(game)
+
+	game = Game()
+	game.name = "Half-Life 2"
+	game.year = 2004
+	game.theme = "FPS"
+	game.publisher = "Valve"
+	db.session.add(game)
 
 	publisher = Publisher()
 	publisher.name = "CD Projekt RED"
 	publisher.country = "Poland"
 	publisher.year = 1994
 	publisher.num_franchises = 1
-
-	db.session.add(game)
 	db.session.add(publisher)
+
+	publisher = Publisher()
+	publisher.name = "Bandai Namco"
+	publisher.country = "Japan"
+	publisher.year = 2015
+	publisher.num_franchises = 1
+	db.session.add(publisher)
+
+	publisher = Publisher()
+	publisher.name = "Valve"
+	publisher.country = "USA"
+	publisher.year = 1996
+	publisher.num_franchises = 1
+	db.session.add(publisher)
+
+	genre = Genre()
+	genre.name = "Action RPG"
+	genre.num_games = 2
+	genre.avg_rating = 9
+	genre.popularity = 1
+	db.session.add(genre)
+
+	genre = Genre()
+	genre.name = "Adventure"
+	genre.num_games = 4
+	genre.avg_rating = 8
+	genre.popularity = 3
+	db.session.add(genre)
+
+	genre = Genre()
+	genre.name = "FPS"
+	genre.num_games = 2
+	genre.avg_rating = 9
+	genre.popularity = 1
+	db.session.add(genre)
+
 	db.session.commit()
 
 	app.run()
