7cf7b08d521f32352469dd9dd0fe44efd8b348be
jfajay <jeff_fajay@utexas.edu>
2017-03-23 16:34:49 -0500

    Finished hardcoding in queries and added in sorting functionality

--- app/static/js/videoGameLookup.js
+++ app/static/js/videoGameLookup.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
   $(function () {
-      $('a[href="#search"]').on('click', function(event) {
-	  event.preventDefault();
-	  $('#search').addClass('open');
-	  $('#search > form > input[type="search"]').focus();
-          $('#but').hide();
-          $('#start-text').hide();
-      });
-      
-      $('#search, #search button.close').on('click keyup', function(event) {
-	  if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
-	      $(this).removeClass('open');
-              $('#but').show();
-	      $('#start-text').show();  
-	  }
-      });
+    $('a[href="#search"]').on('click', function(event) {
+     event.preventDefault();
+     $('#search').addClass('open');
+     $('#search > form > input[type="search"]').focus();
+     $('#but').hide();
+     $('#start-text').hide();
+   });
+    
+    $('#search, #search button.close').on('click keyup', function(event) {
+     if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+       $(this).removeClass('open');
+       $('#but').show();
+       $('#start-text').show();  
+     }
+   });
 
-      $('form').submit(function(event) {
-          var searchstring = $('searchfield');
-   	  searchstring.focus();
- 	  alert(searchstring.val());
-      });
+    $('form').submit(function(event) {
+      var searchstring = $('searchfield');
+      searchstring.focus();
+      alert(searchstring.val());
+    });
   });
 
   $(function () {
@@ -29,6 +29,9 @@ $(document).ready(function(){
       $('#but').hide();
       $('#start-text').hide();
     });
+  });
+  $(function(){
+    $('#tb').tablesorter(); 
   });
 
 });
