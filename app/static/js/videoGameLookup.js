2b4f4976aa085cea2665e024274264fa071f24b7
jfajay <jeff_fajay@utexas.edu>
2017-03-23 16:43:55 -0500

    Dealing with merge for videogamelookup.js

--- app/static/js/videoGameLookup.js
+++ app/static/js/videoGameLookup.js
@@ -1,35 +1,36 @@
 $(document).ready(function(){
   $(function () {
-    $('a[href="#search"]').on('click', function(event) {
-     event.preventDefault();
-     $('#search').addClass('open');
-     $('#search > form > input[type="search"]').focus();
-     $('#but').hide();
-     $('#start-text').hide();
-   });
-    
-    $('#search, #search button.close').on('click keyup', function(event) {
-     if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
-       $(this).removeClass('open');
-       $('#but').show();
-       $('#start-text').show();  
-     }
-   });
+      $('a[href="#search"]').on('click', function(event) {
+    event.preventDefault();
+    $('#search').addClass('open');
+    $('#search > form > input[type="search"]').focus();
+          $('#but').fadeOut();
+          $('#start-text').fadeOut();
+      });
+      
+      $('#search, #search button.close').on('click keyup', function(event) {
+    if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+        $(this).removeClass('open');
+              $('#but').fadeIn();
+        $('#start-text').fadeIn();  
+    }
+      });
 
-    $('form').submit(function(event) {
-      var searchstring = $('searchfield');
+      $('form').submit(function(event) {
+          var searchstring = $('searchfield');
       searchstring.focus();
-      alert(searchstring.val());
-    });
+    alert(searchstring.val());
+      });
   });
 
   $(function () {
     $("#but").click(function () {
       $('#search').trigger('click');
-      $('#but').hide();
-      $('#start-text').hide();
+      $('#but').fadeOut();
+      $('#start-text').fadeOut();
     });
   });
+  
   $(function(){
     $('#tb').tablesorter(); 
   });
