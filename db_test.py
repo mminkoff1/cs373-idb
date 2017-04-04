8bf77337036f1b5dafeab9fc2a4435f79c92be48
Alexander You <alexyou8021@gmail.com>
2017-04-04 16:28:37 -0500

    connecting api to database

--- db_test.py
+++ db_test.py
@@ -32,6 +32,25 @@ engine.connect()
 Session = sessionmaker(bind = engine)
 session = Session()
 
+resp = requests.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=10",
+                     headers={
+					    "X-Mashape-Key": "5nQ6IafYbXmshIQIVdcMhGPqQVhop1NgpLQjsnMyA5A6vqsRXQ"
+					 })
+data = json.loads(resp.text)
+
+for x in data:
+	print x['id']
+        print x['name']
+        game = Game(x['name'], x['id'])
+	session.add(game)
+try:
+	session.commit()
+	print "1"
+except:
+	session.rollback()
+	print "test"
+        
+"""
 game = Game("Minecraft", 2)
 
 try:
@@ -45,27 +64,4 @@ except:
 result = session.query(Game).filter_by(name='Portal').first()
 
 print result
-
-"""
-resp = requests.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=10",
-                     headers={
-					    "X-Mashape-Key": "5nQ6IafYbXmshIQIVdcMhGPqQVhop1NgpLQjsnMyA5A6vqsRXQ"
-					 })
-print (resp.status_code)
-
-data = json.loads(resp.text)
-
-for x in data:
-	print x
-
-game = Game(10)
-
-try:
-	session.add(game)
-	session.commit()
-	print "1"
-except:
-	session.rollback()
-	print "test"
-
 """
